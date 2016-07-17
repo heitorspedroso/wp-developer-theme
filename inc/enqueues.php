ffa32ab3d08071654805b4f8dde90ca368216ab8
Heitor Pedroso <heitorspedroso@gmail.com>
2016-07-17 19:13:35 -0300

    Version 1.0

--- inc/enqueues.php
+++ inc/enqueues.php
@@ -3,7 +3,7 @@
 //Include the enqueue header CSS
 function wp_developer_theme_styles(){
     wp_enqueue_style( 'styles-bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css');
-    wp_enqueue_style( 'styles-bxslider',  get_template_directory_uri() . '/css/jquery.bxslider.css');
+    wp_enqueue_style( 'styles-bxslider',  get_template_directory_uri() . '/css/jquerybxslider.css');
     wp_enqueue_style( 'styles-theme',  get_template_directory_uri() . '/style.css');
 }
 add_action( 'wp_enqueue_scripts', 'wp_developer_theme_styles' );
@@ -11,7 +11,7 @@ add_action( 'wp_enqueue_scripts', 'wp_developer_theme_styles' );
 //Include the enqueue footer SCRIPTS
 function wp_developer_theme_scripts(){
     wp_enqueue_script( 'bootstrap-script','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js','','',true);
-    wp_enqueue_script('bxslider', get_template_directory_uri() . '/js/jquery.bxslider.js', '', '',true);
+    wp_enqueue_script('bxslider', get_template_directory_uri() . '/js/jquerybxslider.js', '', '',true);
     wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', '', '',true);
 }
 add_action( 'wp_enqueue_scripts', 'wp_developer_theme_scripts' );
