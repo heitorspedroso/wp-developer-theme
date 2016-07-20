9327eff854c065cfc72025b34953d4bcd52750f2
Heitor Pedroso <heitorspedroso@gmail.com>
2016-07-19 21:06:18 -0300

    Version 1.0

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
         <?php wp_nav_menu( array(
             'theme_location' => 'primary',
             'menu_id' => 'primary-menu',
-            'menu_class' => 'nav navbar-nav',
+            'menu_class' => 'nav navbar-nav',//or fluid: .nav-justified
             'container'       => false,
             'items_wrap'      => '<ul id="%1$s" class="%2$s nav">%3$s</ul>',
             'walker'          => new wp_bootstrap_navwalker
