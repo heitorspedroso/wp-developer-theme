8fca80a7ce8fcdf012be168f5a09efba6a726b0a
Heitor Pedroso <heitorspedroso@gmail.com>
2016-12-04 21:19:22 -0200

    Version 1.01

--- inc/theme_functions.php
+++ inc/theme_functions.php
@@ -10,3 +10,17 @@ show_admin_bar(false);
 
 //Add Theme Support Thumbnail
 add_theme_support( 'post-thumbnails' );
+
+//Register Widget Default
+function wp_developer_theme_widget_init() {
+    register_sidebar( array(
+        'name'          => __( 'Sidebar', 'Default' ),
+        'id'            => 'sidebar-1',
+        'description'   => '',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ) );
+}
+add_action( 'widgets_init', 'wp_developer_theme_widget_init' );
