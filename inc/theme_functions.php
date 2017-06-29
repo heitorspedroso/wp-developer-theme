68e68edab553cefcce5c6bc32f447f0b7944500f
Heitor Pedroso <heitorspedroso@gmail.com>
2017-06-29 20:33:23 -0300

    Insert function character limit

--- inc/theme_functions.php
+++ inc/theme_functions.php
@@ -24,3 +24,16 @@ function wp_developer_theme_widgets_init() {
 	) );
 }
 add_action( 'widgets_init', 'wp_developer_theme_widgets_init' );
+
+//Caracter Limit
+function excerpt($limit) {
+$excerpt = explode(' ', get_the_excerpt(), $limit);
+if (count($excerpt)>=$limit) {
+array_pop($excerpt);
+$excerpt = implode(" ",$excerpt).'...';
+} else {
+$excerpt = implode(" ",$excerpt);
+}
+$excerpt = preg_replace('`\[[^\]]*\]`','',$excerpt);
+return $excerpt;
+}
