aff1c56c8328193723637ceb00f843ae927f7db3
Heitor Pedroso <heitorspedroso@gmail.com>
2017-06-29 20:50:19 -0300

    Version 1.0.1
    remove comments code the header

--- front-page.php
+++ front-page.php
@@ -1,14 +1,4 @@
 <?php
-/*
-  WPDEVELOPERTHEME HELP: BOOSTRAP EXAMPLE
-  <div class="container"> or container-fluid
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-
-        </div>
-    </div>
-  </div>
-*/
 get_header();
 
 get_footer();
