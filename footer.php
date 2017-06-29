3f378476d112bd3faf6333c342655c28f7d413b0
Heitor Pedroso <heitorspedroso@gmail.com>
2017-06-29 20:49:57 -0300

    Version 1.0.1
    remove comments code the header

--- footer.php
+++ footer.php
@@ -1,15 +1,3 @@
-<?php
-/*
-  WPDEVELOPERTHEME HELP: BOOSTRAP EXAMPLE
-  <div class="container"> or container-fluid
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-
-        </div>
-    </div>
-  </div>
-*/
-?>
 <?php wp_footer(); ?>
 </body>
 </html>
