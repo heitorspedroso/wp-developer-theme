9af1d9c06006d29b644dab15aaf491ee0b042354
Heitor Pedroso <heitorspedroso@gmail.com>
2021-03-23 19:55:27 -0300

    update comments template

--- comments.php
+++ comments.php
@@ -1,63 +1,79 @@
 <?php
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password,
+ * return early without loading the comments.
+ */
 if ( post_password_required() ) {
-	return;
+    return;
 }
+
+$comment_count = get_comments_number();
 ?>
 
-<div id="comments" class="comments-area">
+<div id="comments" class="comments-area default-max-width <?php echo get_option( 'show_avatars' ) ? 'show-avatars' : ''; ?>">
 
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				$comments_number = get_comments_number();
-				if ( 1 === $comments_number ) {
-					/* translators: %s: post title */
-					printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'twentysixteen' ), get_the_title() );
-				} else {
-					printf(
-						/* translators: 1: number of comments, 2: post title */
-						_nx(
-							'%1$s thought on &ldquo;%2$s&rdquo;',
-							'%1$s thoughts on &ldquo;%2$s&rdquo;',
-							$comments_number,
-							'comments title',
-							'twentysixteen'
-						),
-						number_format_i18n( $comments_number ),
-						get_the_title()
-					);
-				}
-			?>
-		</h2>
+    <?php
+    if ( have_comments() ) :
+        ;
+        ?>
+        <h2 class="comments-title">
+            <?php if ( '1' === $comment_count ) : ?>
+                <?php esc_html_e( '1 comment', 'wp-developer-theme' ); ?>
+            <?php else : ?>
+                <?php
+                printf(
+                /* translators: %s: Comment count number. */
+                    esc_html( _nx( '%s comment', '%s comments', $comment_count, 'Comments title', 'wp-developer-theme' ) ),
+                    esc_html( number_format_i18n( $comment_count ) )
+                );
+                ?>
+            <?php endif; ?>
+        </h2><!-- .comments-title -->
 
-		<?php the_comments_navigation(); ?>
+        <ol class="comment-list">
+            <?php
+            wp_list_comments(
+                array(
+                    'avatar_size' => 60,
+                    'style'       => 'ol',
+                    'short_ping'  => true,
+                )
+            );
+            ?>
+        </ol><!-- .comment-list -->
 
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'       => 'ol',
-					'short_ping'  => true,
-					'avatar_size' => 42,
-				) );
-			?>
-		</ol><!-- .comment-list -->
+        <?php
+        the_comments_pagination(
+            array(
+                'before_page_number' => esc_html__( 'Page', 'wp-developer-theme' ) . ' ',
+                'mid_size'           => 0,
+                'prev_text'          => sprintf(
+                    '%s <span class="nav-prev-text">%s</span>',
+                    esc_html__( 'Older comments', 'wp-developer-theme' )
+                ),
+                'next_text'          => sprintf(
+                    '<span class="nav-next-text">%s</span> %s',
+                    esc_html__( 'Newer comments', 'wp-developer-theme' )
+                ),
+            )
+        );
+        ?>
 
-		<?php the_comments_navigation(); ?>
+        <?php if ( ! comments_open() ) : ?>
+        <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'wp-developer-theme' ); ?></p>
+    <?php endif; ?>
+    <?php endif; ?>
 
-	<?php endif; // Check for have_comments(). ?>
+    <?php
+    comment_form(
+        array(
+            'logged_in_as'       => null,
+            'title_reply'        => esc_html__( 'Compartilhe seu comentário com a gente!', 'wp-developer-theme' ),
+            'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+            'title_reply_after'  => '</h2>',
+        )
+    );
+    ?>
 
-	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'twentysixteen' ); ?></p>
-	<?php endif; ?>
-
-	<?php
-		comment_form( array(
-			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
-			'title_reply_after'  => '</h2>',
-		) );
-	?>
-
-</div><!-- .comments-area -->
+</div><!-- #comments -->
